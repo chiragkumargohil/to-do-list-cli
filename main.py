89a20ab72e6e47bbb592c1674df4518a4a41906c
Chiragkumar Gohil <108747654+chiragkumargohil@users.noreply.github.com>
2022-07-18 22:24:40 +0530

    Update main.py
    Used Objected Oriented Programming.

--- main.py
+++ main.py
@@ -1,48 +1,68 @@
-from importlib.resources import contents
+class ToDoList:
 
+    def __init__(self, li):
+        self.items = li
 
-user_input = ""
-to_do_list = []
-
-print('''"Welcome to To-Do List App"''')
-def menu_list():
-    print("\nPress '1' to Add Item.\nPress '2' to Remove Item.\nPress '3' to View the List.\nPress '4' to Quit the App\n")
-
-with open("to-do-list.txt") as f:
-    lines = f.readlines()
-    for line in lines:
-        line.strip("")
-        to_do_list.append(line.strip("\n"))
-
-while user_input != "4":
-    menu_list()
-    user_input = input("What do you want to Do? ")
+    def displayItems(self):
+        print("\n|| Your To-Do List ||\n")
+        for i, item in enumerate(self.items):
+            print(f"{i+1}. {item}")
     
-    if user_input == "1":
-        item = input("\nAdd: ")
-        to_do_list.append(item)
-        print(f"Item is added.")
-    elif user_input == "2":
-        c = len(to_do_list)
-        print("\nWhat do you want remove?")
-        for item in range(1, (c+1)):
-            print(f"{item}. {to_do_list[item-1]}")
-        item = input("Remove: ")
-        if item in to_do_list:
-            to_do_list.remove(item)
-            print(f"Item is removed successfully.")
+    def addItem(self):
+        item = input("\nAdd : ")
+        self.items.append(item)
+        print(f"Item has been added.")
+
+    def removeItem(self):
+        ToDoList.displayItems(self)
+        print("\nWhat do you want remove from above items?")
+        try:
+            i = int(input("Remove (Just type assigned number) : "))
+            self.items.remove(self.items[i - 1])
+            print(f"Item has been removed successfully.")
+        except:
+            print("Please enter a number assigned to item in above list.")
+
+    @staticmethod
+    def menu_list():
+        menu_message ='''
+Press '1' to Show Items
+Press '2' to Add Item
+Press '3' to Remove Item
+Press '4' to Quit the App
+'''
+        print(menu_message)
+
+if __name__ == "__main__":
+    user_input = ""
+    to_do_list = []
+
+    print('''"Welcome to To-Do List App"''')
+
+    with open("to-do-list.txt") as f:
+        lines = f.readlines()
+        for line in lines:
+            line.strip("")
+            to_do_list.append(line.strip("\n"))
+
+    listing = ToDoList(to_do_list)
+
+    while user_input != "4":
+
+        ToDoList.menu_list()
+        user_input = input("What do you want to Do? ")
+        
+        if user_input == "1":
+            listing.displayItems()
+        elif user_input == "2":
+            listing.addItem()
+        elif user_input == "3":
+            listing.removeItem()
+        elif user_input == "4":
+            print("\nThank You! Your To-Do Lists have been saved.")
         else:
-            print("\nItem is not in your To-Do List.")
-    elif user_input == "3":
-        c = len(to_do_list)
-        print("\nYour To-Do Lists:")
-        for item in range(1, (c+1)):
-            print(f"{item}. {to_do_list[item-1]}")
-    elif user_input == "4":
-        print("\nThank You! Your To-Do Lists have been saved.")
-    else: 
-        print("\nEnter valid response.")
-    
-with open("to-do-list.txt", "w") as f:
-    for item in to_do_list:
-        print(item, file=f)
+            print("\nEnter valid response.")
+        
+        with open("to-do-list.txt", "w") as f:
+            for item in to_do_list:
+                print(item, file=f)
